8824c783b207059b93a8d827afc7a1f9a043ed4c
Xiaoyu Lu <xlu14@slb.com>
2020-11-20 16:27:22 +0800

    refactor: long method: extract method RenderInputList

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -30,15 +30,21 @@ namespace WordFrequency
 
             inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
 
-            List<string> strList = new List<string>();
+            List<string> strList = RenderInputList(inputList);
 
+            return string.Join("\n", strList.ToArray());
+        }
+
+        private static List<string> RenderInputList(List<Input> inputList)
+        {
+            List<string> strList = new List<string>();
             foreach (Input w in inputList)
             {
                 string s = w.Value + " " + w.WordCount;
                 strList.Add(s);
             }
 
-            return string.Join("\n", strList.ToArray());
+            return strList;
         }
 
         private static List<Input> SplitInputToList(string inputStr)
